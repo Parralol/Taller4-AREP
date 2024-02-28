7ab7c4e4632d43ab3260fb6364827be4e2107ff4
Parralol <santinico36@hotmail.com>
2024-02-28 11:13:18 -0500

    last

--- src/test/java/edu/escuelaing/arem/ASE/app/AppTest.java
+++ src/test/java/edu/escuelaing/arem/ASE/app/AppTest.java
@@ -15,26 +15,20 @@ public class AppTest{
     @Test
     public void testGetFile() throws IOException  {
         FileController fileController = new FileController();
-        // Assuming "sampleFile.txt" exists for testing
         String result = fileController.getFile("eba.png");
         assertNotNull(result);
-        // Add more assertions based on your actual implementation
     }
 
     @Test
     public void testSearchFile() throws IOException {
         FileController fileController = new FileController();
-        // Assuming "sampleFile.txt" exists for testing
         String result = fileController.searchFile("sampleFile.txt");
         assertNotNull(result);
-        // Add more assertions based on your actual implementation
     }
     @Test
     public void testGetImage() throws IOException {
         FileController fileController = new FileController();
-        // Assuming "sampleImage.jpg" exists for testing
         byte[] result = fileController.getImage("eba.png");
         assertNotNull(result);
-        // Add more assertions based on your actual implementation
     }
 }
